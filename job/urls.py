7faecdd126db1c1103b244a966f18d340e4138ae
3zoz <iamazoz601@gmail.com>
2020-08-22 16:40:55 +0200

    --- Auto-Git Commit ---

--- job/urls.py
+++ job/urls.py
@@ -4,5 +4,7 @@ app_name='job' #lazem tktp asm al app
 
 urlpatterns = [
     path('', views.job_list),
+    path('', views.job_list_num, name='List'),
+
     path('<int:id>', views.job_detail, name='job_detail'),
 ]
--- job/views.py
+++ job/views.py
@@ -1,13 +1,25 @@
 from django.shortcuts import render
 from .models import job
+from django.core.paginator import Paginator
+
 # Create your views here.
 
 def job_list(request):
     job_list = job.objects.all()
-    context = { 'jobs' : job_list}
+
+    paginator = Paginator(job_list, 1) # Show 1 contacts per page.
+    page_number = request.GET.get('page')
+    page_obj = paginator.get_page(page_number)
+
+
+    context = { 'jobs' : page_obj}
     return render(request, 'job/job_list.html', context)
 
+def job_list_num(request):
+    job_list = job.objects.all()
 
+    context = { 'jobs' : page_obj}
+    return count(job_list)
 
 def job_detail(request, id):
     job_detail = job.objects.get(id=id)
