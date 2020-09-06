b01f51a4c1f000a499c4f050836c2a332b79bb3c
3zoz <iamazoz601@gmail.com>
2020-09-07 00:56:15 +0200

    --- Auto-Git Commit ---

--- accounts/models.py
+++ accounts/models.py
@@ -7,11 +7,16 @@ from django.db.models.signals import post_save
 
 # Create your models here.
 
+def image_upload(instance, file_name):
+    imagename, extension = file_name.split(".")
+    return "prof/%s.%s"%(instance.id, extension)
+
+
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     city = models.ForeignKey("City", verbose_name=("user_city"), on_delete=models.CASCADE, blank=True, null=True)
     phone_number = models.CharField(max_length=15)
-    image = models.ImageField(upload_to=None, height_field=None, width_field=None, max_length=None)
+    image = models.ImageField(upload_to=image_upload, height_field=None, width_field=None, max_length=None)
 
     def __str__(self):
         return str(self.user)
@@ -24,3 +29,5 @@ def create_user_profile(sender, instance, created, **kwargs):
 
 class City(models.Model):
     name = models.name = models.CharField(max_length=30)
+
+
