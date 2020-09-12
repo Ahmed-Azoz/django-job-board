4b5ae07cb3201762a6023aadbe663a32ee6f3550
3zoz <iamazoz601@gmail.com>
2020-09-12 20:47:06 +0200

    --- Auto-Git Commit ---

--- job/filter.py
+++ job/filter.py
@@ -2,7 +2,9 @@ import django_filters
 from .models import job
 
 class JobFilter(django_filters.FilterSet):
+    #3shan afilter bklma mwgoda dmn l descraption
+    description = django_filters.CharFilter(lookup_expr='icontains')
     class Meta:
         model = job
         fields = '__all__'
-        exclude = ['published_at', 'vacancy', 'salary', 'image']
+        exclude = ['published_at', 'vacancy', 'salary', 'image', 'slug', 'owner']
