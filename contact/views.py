bd2e5f4cd79abd36978632ff5195b2d194426e42
3zoz <iamazoz601@gmail.com>
2020-09-13 01:54:04 +0200

    --- Auto-Git Commit ---

--- contact/views.py
+++ contact/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
-
+from .models import info
 # Create your views here.
 def send_massage(request):
-    pass
+    myinfo = info.objects.first
+    return render(request, 'contact/contact.html', {'myinfo': myinfo})
--- job/filter.py
+++ job/filter.py
@@ -4,6 +4,8 @@ from .models import job
 class JobFilter(django_filters.FilterSet):
     #3shan afilter bklma mwgoda dmn l descraption
     description = django_filters.CharFilter(lookup_expr='icontains')
+    title = django_filters.CharFilter(lookup_expr='icontains')
+
     class Meta:
         model = job
         fields = '__all__'
